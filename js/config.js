0c1bb47638a1b27eea9ba333ac8d2985e849ff4b
Sunwoo Jeong <swjeong0825@gmail.com>
2026-04-06 08:37:42 -0700

    nit: update chat bse url

--- js/config.js
+++ js/config.js
@@ -1,8 +1,10 @@
+const CHAT_API_BASE_URL = "https://tlmbchattointent-production.up.railway.app";
+
 (function () {
   var params = new URLSearchParams(window.location.search);
   var fromQuery = params.get("chatApi");
   // Must be an absolute origin (include https://). Host-only strings are turned into https:// in app.js.
   window.TLCHAT_CONFIG = {
-    chatApiBaseUrl: (fromQuery || "https://tlmbchattointent-production.up.railway.app").replace(/\/$/, ""),
+    chatApiBaseUrl: (fromQuery || CHAT_API_BASE_URL).replace(/\/$/, ""),
   };
 })();
