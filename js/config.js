9f94b1778671406dc6b34c20f7e61c87f6c28cbf
Sunwoo Jeong <swjeong0825@gmail.com>
2026-05-26 14:52:04 -0700

    prod endpoint

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
-// const CHAT_API_BASE_URL = "https://tlmbchattointent-production.up.railway.app";
-const CHAT_API_BASE_URL = "https://tlmbchattointent-copy2-production.up.railway.app"; // new feature testing
+const CHAT_API_BASE_URL = "https://tlmbchattointent-production.up.railway.app";
+// const CHAT_API_BASE_URL = "https://tlmbchattointent-copy2-production.up.railway.app"; // new feature testing
 
 // const CHAT_API_BASE_URL = "http://127.0.0.1:8000";
 
-// const BACKEND_MAIN_BASE_URL = "https://tlmbbackendmain-production.up.railway.app";
-const BACKEND_MAIN_BASE_URL = "https://tlmbbackendmain-copy2-production.up.railway.app"; // new feature testing
+const BACKEND_MAIN_BASE_URL = "https://tlmbbackendmain-production.up.railway.app";
+// const BACKEND_MAIN_BASE_URL = "https://tlmbbackendmain-copy2-production.up.railway.app"; // new feature testing
 
 // const BACKEND_MAIN_BASE_URL = "http://127.0.0.1:8000";
 
